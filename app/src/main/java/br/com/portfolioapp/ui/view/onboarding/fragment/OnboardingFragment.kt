83175894072827a82fdbd0acc97469a56242463a
Agnaldo Rodrigues de Carvalho <ag.carvalho@guide.com.br>
2022-01-19 23:48:15 -0300

    Implements navigation from OnboardingFragment to RegisterFragment using jetpack navigation component
    Signed-off-by: Agnaldo Rodrigues de Carvalho <ag.carvalho@guide.com.br>

--- app/src/main/java/br/com/portfolioapp/ui/view/onboarding/fragment/OnboardingFragment.kt
+++ app/src/main/java/br/com/portfolioapp/ui/view/onboarding/fragment/OnboardingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import br.com.portfolioapp.R
 import br.com.portfolioapp.databinding.FragmentOnboardingBinding
 import br.com.portfolioapp.ui.view.BaseFragment
@@ -15,5 +16,17 @@ class OnboardingFragment: BaseFragment<FragmentOnboardingBinding>() {
         savedInstanceState: Bundle?
     ): View {
         return bindView(inflater, container, R.layout.fragment_onboarding)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btnLetsGo.setOnClickListener { view ->
+            val directions = OnboardingFragmentDirections.actionOnboardingFragmentToRegisterFragment()
+            view.findNavController().navigate(directions)
+        }
     }
 }
